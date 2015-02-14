9b99d486d3c740f25779de2922871df66c263dcd
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-14 12:42:17 +0530

    Bump versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Exposing Internal Meteor Apis to Hack Meteor Easily",
-  "version": "1.3.0",
+  "version": "1.3.1",
   "git": "https://github.com/meteorhacks/meteorx.git",
   "name": "meteorhacks:meteorx"
 });
